7faec4234b5669d9fd005915a97cf38ea6425aff
Yanchen Huang <90331527+02hyc@users.noreply.github.com>
2025-05-16 00:33:48 +0800

    fix author info

--- debert_dataloader.py
+++ debert_dataloader.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 '''
-@author: Yongchang Cao
-@contact: cyc990520@gmail.com
-@file: debert_dataloader.py
-@time: 2022/3/22 21:35
+@author: Yanchen Huang
+@contact: yanchenhuang@smail.nju.edu.cn
+@time: 2025/3/23
 @desc:
 '''
 import os
